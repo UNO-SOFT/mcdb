9b7590e079a57fd68acab0d69ec9556846877b12
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2021-09-06 11:03:34 +0200

    Add test

--- mcdb.go
+++ mcdb.go
@@ -9,18 +9,17 @@
 package mcdb
 
 import (
-    "errors"
+	"errors"
 	"fmt"
 	"hash"
 	"hash/fnv"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
 
-    "golang.org/x/exp/mmap"
 	"github.com/colinmarc/cdb"
+	"golang.org/x/exp/mmap"
 )
 
 var hashesPool = sync.Pool{New: func() interface{} { return fnv.New32() }}
@@ -35,6 +34,11 @@ func Hash(p []byte) uint32 {
 	return res
 }
 
+// bucket returns the specific bucket a key must reside in.
+func bucket(key []byte, expC int) int {
+	return int(Hash(key) >> expC)
+}
+
 // Writer is the writer. It needs the number of tables beforehand.
 type Writer struct {
 	ws   []*cdb.Writer
@@ -42,6 +46,9 @@ type Writer struct {
 }
 
 // NewWriter returns a new Writer.
+//
+// The next power-of-two number of tables are created, so for example
+// with n=3, 4 tables are created, containing maximum 16GiB of data.
 func NewWriter(dir string, n int) (*Writer, error) {
 	var n2 int
 	expC := 32
@@ -65,6 +72,7 @@ func NewWriter(dir string, n int) (*Writer, error) {
 	}
 	return &m, nil
 }
+
 // Close the underlying writers.
 func (m *Writer) Close() error {
 	if m == nil || len(m.ws) == 0 {
@@ -79,12 +87,10 @@ func (m *Writer) Close() error {
 	}
 	return nil
 }
+
 // Put the key into one of the underlying writers.
 func (m *Writer) Put(key, val []byte) error {
 	return m.ws[bucket(key, m.expC)].Put(key, val)
-}
-func bucket(key []byte, expC int) int {
-	return int(Hash(key) >> expC)
 }
 
 // Reader is a reader for multiple CDB files.
@@ -105,10 +111,10 @@ func NewReader(dir string) (*Reader, error) {
 		if !(strings.HasPrefix(nm, "mcdb-") && strings.HasSuffix(nm, ".cdb")) {
 			continue
 		}
-        var u1, u2 uint32
-        if _, err := fmt.Sscanf(de.Name(), FileName, &u1, &u2); err != nil {
-            return nil, fmt.Errorf("%s: %w", de.Name(), err)
-        }
+		var u1, u2 uint32
+		if _, err := fmt.Sscanf(de.Name(), FileName, &u1, &u2); err != nil {
+			return nil, fmt.Errorf("%s: %w", de.Name(), err)
+		}
 		if m.rs == nil {
 			m.rs = make([]*cdb.CDB, int(u1))
 			for i := 1; i < len(m.rs); i <<= 1 {
@@ -116,14 +122,14 @@ func NewReader(dir string) (*Reader, error) {
 			}
 		}
 		if u1 != uint32(len(m.rs)) {
-            _ = m.Close()
-            return nil, fmt.Errorf("%s: first number should be the same for all files", de.Name())
+			_ = m.Close()
+			return nil, fmt.Errorf("%s: first number should be the same for all files", de.Name())
 		}
-		if u1 > u2 {
-            _ = m.Close()
-            return nil, fmt.Errorf("%s: first number should not be bigger than the second", de.Name())
+		if u1 < u2 {
+			_ = m.Close()
+			return nil, fmt.Errorf("%s: second number should not be bigger than the second", de.Name())
 		}
-        fh, err := mmap.Open(filepath.Join(dir, de.Name()))
+		fh, err := mmap.Open(filepath.Join(dir, de.Name()))
 		if err != nil {
 			_ = m.Close()
 			return nil, err
@@ -133,16 +139,16 @@ func NewReader(dir string) (*Reader, error) {
 			return nil, err
 		}
 	}
-    if len(m.rs) == 0 {
-        return nil, errors.New("no "+FileName+" files found")
-    }
-    for i, r := range m.rs {
-        if r == nil {
-            _ = m.Close()
-        return nil, fmt.Errorf(FileName +" not found", len(m.rs), i)
-        }
-    }
-	log.Println("rs:", len(m.rs), "expC:", m.expC)
+	if len(m.rs) == 0 {
+		return nil, errors.New("no " + FileName + " files found")
+	}
+	for i, r := range m.rs {
+		if r == nil {
+			_ = m.Close()
+			return nil, fmt.Errorf(FileName+" not found", len(m.rs), i)
+		}
+	}
+	//log.Println("rs:", len(m.rs), "expC:", m.expC)
 	return &m, nil
 }
 
@@ -167,6 +173,7 @@ func (m *Reader) Close() error {
 func (m *Reader) Get(key []byte) ([]byte, error) {
 	return m.rs[bucket(key, m.expC)].Get(key)
 }
+
 // Iter returns an iterator.
 func (m *Reader) Iter() *Iterator {
 	return &Iterator{m: m, it: m.rs[0].Iter()}
@@ -180,11 +187,14 @@ type Iterator struct {
 }
 
 // Err returns the last error.
-func (m *Iterator) Err() error    { return m.it.Err() }
+func (m *Iterator) Err() error { return m.it.Err() }
+
 // Key returns the current key (after Next).
-func (m *Iterator) Key() []byte   { return m.it.Key() }
+func (m *Iterator) Key() []byte { return m.it.Key() }
+
 // Value returns the current value (after Next).
 func (m *Iterator) Value() []byte { return m.it.Value() }
+
 // Next advances the iterator, if possible.
 func (m *Iterator) Next() bool {
 	if m.it.Next() {
@@ -199,4 +209,3 @@ func (m *Iterator) Next() bool {
 	}
 	return false
 }
-
---
+++ mcdb_test.go
@@ -0,0 +1,75 @@
+package mcdb_test
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/UNO-SOFT/mcdb"
+)
+
+func TestReadWrite(t *testing.T) {
+	dn, err := os.MkdirTemp("", "mcdb-test.cdb")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dn)
+	cw, err := mcdb.NewWriter(dn, 4)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cw.Close()
+
+	keys := make([][]byte, 256)
+	for i := range keys {
+		keys[i] = []byte{byte(i)}
+		if err = cw.Put(keys[i], keys[i]); err != nil {
+			t.Fatal(err)
+		}
+	}
+	if err = cw.Close(); err != nil {
+		t.Error(err)
+	}
+
+	rw, err := mcdb.NewReader(dn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rw.Close()
+	for _, want := range keys {
+		got, err := rw.Get(want)
+		if err != nil {
+			t.Errorf("%q: %w", want, err)
+		}
+		if !bytes.Equal(got, want) {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	}
+
+	it := rw.Iter()
+	for it.Next() {
+		if err = it.Err(); err != nil {
+			t.Fatal(err)
+		}
+		key, val := it.Key(), it.Value()
+		if len(key) != 1 {
+			t.Errorf("key=%q too long", key)
+		}
+		if !bytes.Equal(key, val) {
+			t.Errorf("key=%q val=%q", key, val)
+		}
+		keys[int(key[0])] = nil
+	}
+	if err = it.Err(); err != nil {
+		t.Error(err)
+	}
+	if err = rw.Close(); err != nil {
+		t.Error(err)
+	}
+
+	for i, k := range keys {
+		if k != nil {
+			t.Errorf("%d left out from iteration", i)
+		}
+	}
+}
